375a9b8047676b94e90d62cd76950a5edadec399
Libamlak Birhanu <realweirdo904@gmail.com>
2022-08-29 14:37:57 +0300

    protected routes and asigned routes based on role

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Container } from "@mantine/core";
 import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 import Layout from "./components/layout/Layout";
 import About from "./pages/about";
 import Index from "./pages/index";
@@ -8,18 +9,45 @@ import Detail from "./pages/detail";
 import Login from "./pages/login";
 import Register from "./pages/register";
 import Home from "./pages/home";
+import Products from "./pages/products";
+import Unauthorized from "./pages/unauthorized";
+import CAN from "@ability/can";
 
 function App() {
+  const auth = useSelector((state) => state.auth);
+  
   return (
     <Layout>
       <Container size="xl">
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="home" element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="detail" element={<Detail />} />
+          <Route
+            path="home"
+            element={!auth.user ? <Navigate replace to="/login" /> : <Home />}
+          />
+          <Route
+            path="about"
+            element={!auth.user ? <Navigate replace to="/login" /> : <About />}
+          />
+          <Route
+            path="products"
+            element={
+              !auth.user ? (
+                <Navigate replace to="/login" />
+              ) : !CAN("read", "store") ? (
+                <Navigate replace to="/unauthorized" />
+              ) : (
+                <Products />
+              )
+            }
+          />
+          <Route
+            path="detail"
+            element={!auth.user ? <Navigate replace to="/login" /> : <Detail />}
+          />
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
+          <Route path="unauthorized" element={<Unauthorized />} />
         </Routes>
       </Container>
     </Layout>
